14975ef7ee4dd87a65b6078736ce73802e8828dd
Roger Jungemann <roger@teamsketchy.com>
2021-06-14 17:06:01 -0700

    1x, 16x, and 64x

--- libraries/chain/database.cpp
+++ libraries/chain/database.cpp
@@ -1093,7 +1093,7 @@ fc::sha256 database::get_pow_target()const
    uint32_t head_num = head_block_num();
    if (head_num < 600000)
    {
-      fc::sha256 target = fc::sha256("000000ffff000000000000000000000000000000000000000000000000000000");
+      fc::sha256 target = fc::sha256("00000ffff0000000000000000000000000000000000000000000000000000000");
       wlog("database::get_pow_target ${t}", ("t",target));
       return target;
    }
@@ -1105,9 +1105,14 @@ fc::sha256 database::get_pow_target()const
    }
    else
    {
+      fc::sha256 target = fc::sha256("0000000ffff00000000000000000000000000000000000000000000000000000");
+      wlog("database::get_pow_target ${t}", ("t",target));
+      return target;
+      /*
       const auto& dgp = get_dynamic_global_properties();
       wlog("database::get_pow_target ${t}", ("t",dgp.mining_target));
       return dgp.mining_target;
+      */
    }
 }
 
