8ac1f35c831c1f93392cc5babce30c28ffe134a6
James Tucker <jftucker@gmail.com>
2021-10-26 20:27:37 -0700

    seeds: update seed list with additions & removals

--- libraries/plugins/p2p/include/xgt/plugins/p2p/p2p_default_seeds.hpp
+++ libraries/plugins/p2p/include/xgt/plugins/p2p/p2p_default_seeds.hpp
@@ -9,12 +9,11 @@ namespace xgt{ namespace plugins { namespace p2p {
 const std::vector< std::string > default_seeds;
 #else
 const std::vector< std::string > default_seeds = {
+    "seed-node-1.xgt.co.in:2001",
     "xgt.rag.pub:2001",
-    "xgt2.rag.pub:2001",
     "68.129.31.2:2001",
     "95.216.69.92:2001",
     "95.216.71.199:2001",
-    "98.33.76.100:2001",
     "116.202.114.157:2001",
     "195.201.167.19:2001"
 };
