013dfa4b8f1a7ca66dd4a4635a6363738d3ac6ba
James Tucker <jftucker@gmail.com>
2022-04-12 22:16:47 -0700

    seeds: update again

--- libraries/plugins/p2p/include/xgt/plugins/p2p/p2p_default_seeds.hpp
+++ libraries/plugins/p2p/include/xgt/plugins/p2p/p2p_default_seeds.hpp
@@ -9,13 +9,14 @@ namespace xgt{ namespace plugins { namespace p2p {
 const std::vector< std::string > default_seeds;
 #else
 const std::vector< std::string > default_seeds = {
+    "mainnet.xgtcrypto.com:2001",
     "seed-node-1.xgt.co.in:2001",
     "xgt.rag.pub:2001",
+    "116.202.114.157:2001",
+    "195.201.167.19:2001",
     "68.129.31.2:2001",
     "95.216.69.92:2001",
-    "95.216.71.199:2001",
-    "116.202.114.157:2001",
-    "195.201.167.19:2001"
+    "95.216.71.199:2001"
 };
 #endif
 
